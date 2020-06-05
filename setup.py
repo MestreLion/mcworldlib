75d670a724443cabf663b84566808e365e1fc286
Ian S. Evans <ievans3024@gmail.com>
2020-06-04 22:24:22 -0500

    Remove commented and unused code

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 
-from importlib import import_module
-from os import path
-
-from setuptools import setup, find_packages
-#
-#
-# packages = find_packages()
-# projname = packages[0]
-# project = import_module(projname)
-# projdir = path.abspath(path.dirname(__file__))
-#
-# with open(path.join(projdir, 'README.md'), encoding='utf-8') as f:
-#     readme = f.read().strip()
+from setuptools import setup
 
 setup()
