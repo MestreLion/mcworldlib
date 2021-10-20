eaa568005b5c05112b008df1f7a867bbdebe2519
MestreLion <linux@rodrigosilva.com>
2021-10-20 15:02:31 -0300

    improve nbt: add Root.__repr__, more typing, exported names

--- mcworldlib/nbt.py
+++ mcworldlib/nbt.py
@@ -10,9 +10,11 @@ Wraps whatever library is used as backend, currently nbtlib
 # No __all__, as being a wrapper it exports all imported names from the backend
 # and all the ones defined here
 
-import io as _io
-import logging as _logging
-import zlib as _zlib
+# Delete ALL imported and declared names not meant for export, see the end of file
+import io
+import logging
+import typing as t
+import zlib
 
 # TODO: (and suggest to nbtlib)
 # - Auto-casting value to tag on assignment based on current type
@@ -37,7 +39,24 @@ from nbtlib.path import Path
 from nbtlib.literal.serializer import serialize_tag as _serialize_tag
 
 
-_log = _logging.getLogger(__name__)
+_log = logging.getLogger(__name__)
+
+# Concrete and (meant to be) instantiable tags, i.e. no Base, Numeric, End, etc
+AnyTag: 't.TypeAlias' = t.Union[
+    Byte,
+    Short,
+    Int,
+    Long,
+    Float,
+    Double,
+    ByteArray,
+    String,
+    List,
+    Compound,
+    IntArray,
+    LongArray,
+]
+T = t.TypeVar('T', bound='Root')
 
 
 class Root(Compound):
@@ -52,22 +71,33 @@ class Root(Compound):
         self.root_name: str = root_name
 
     @property
-    def data_root(self):
-        """The sole root child tag containing all data apart from DataVersion.
+    def data_root(self) -> AnyTag:
+        """Root itself or the child containing all data apart from DataVersion.
 
-        If there is no such tag, return root itself
+        A convenience shortcut for root compounds that contains just DataVersion
+        and another tag, and all relevant data is in that tag, as is the case for
+        most Minecraft data:
+        'Data'  in level.dat (with DataVersion *inside* it)
+        'data'  in <dim>/data/*.dat files (idcounts, map_*, raids*)
+        'Level' in chunks from <dim>/region/*.mca anvil files, 'region' category
+
+        Known NBT with no sole child, data is at root along with DataVersion:
+        - chunks from <dim>/*/*.mca anvil files, 'entities' and 'poi' categories
         """
-        tags = set(self.keys()) - {'DataVersion'}
+        return self._data_root[1]
 
-        # 'Data'  in level.dat (with DataVersion *inside* it)
-        # 'data'  in <dim>/data/*.dat files (idcounts, map_*, raids*)
-        # 'Level' in chunks from <dim>/region/*.mca anvil files, 'region' category
-        if len(tags) == 1:
-            return self[next(iter(tags))]
+    @property
+    def data_root_key(self) -> str:
+        """data_root key, if any"""
+        return self._data_root[0]
 
-        # No sole child, data is at root along with with DataVersion
-        # - chunks from <dim>/*/*.mca anvil files, 'entities' and 'poi' categories
-        return self
+    @property
+    def _data_root(self) -> t.Tuple[str, AnyTag]:
+        tags = self.keys() - 'DataVersion'
+        if not len(tags) == 1:
+            return "", self
+        name = next(iter(tags))
+        return name, self[name]
 
     @property
     def root(self):
@@ -76,7 +106,8 @@ class Root(Compound):
         return self
 
     @classmethod
-    def parse(cls, buff, byteorder='big'):
+    def parse(cls: t.Type[T], buff, byteorder='big') -> T:
+        # For the typing dance, see https://stackoverflow.com/a/44644576/624066
         tag_id = _read_numeric(_BYTE, buff, byteorder)
         if not tag_id == cls.tag_id:
             # Possible issues with a non-Compound root:
@@ -85,17 +116,21 @@ class Root(Compound):
             raise TypeError("Non-Compound root tags is not supported:"
                             f"{cls.get_tag(tag_id).__name__}")
         name = _read_string(buff, byteorder)
-        self = super().parse(buff, byteorder)
+        self: T = super().parse(buff, byteorder)
         self.root_name = name
         return self
 
-    def write(self, buff, byteorder='big'):
+    def write(self, buff, byteorder='big') -> None:
         _write_numeric(_BYTE, self.tag_id, buff, byteorder)
         _write_string(getattr(self, 'root_name', "") or "", buff, byteorder)
         super().write(buff, byteorder)
 
     def __repr__(self):
-        return f'<{self.__class__.__name__} {self.root_name!r}: tags: {len(self)}>'
+        key, data = self._data_root  # save refs for efficiency
+        if key:
+            key = f" ({len(data)} in {key!r})"
+        name = f" {self.root_name!r}" if self.root_name else ""
+        return f'<{self.__class__.__name__}{name} tags: {len(self)}{key}>'
 
 
 # Overrides and extensions
@@ -112,8 +147,11 @@ class File(Root, _File):
     @classmethod
     def load_mcc(cls, filename):
         with open(filename, 'rb') as buff:
-            data = _io.BytesIO(_zlib.decompress(buff.read()))
+            data = io.BytesIO(zlib.decompress(buff.read()))
         return cls.from_buffer(data)
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.filename!r} {self.root!r}>'
 
 
 # Add .pretty() method to all NBT tags
@@ -124,3 +162,10 @@ String.__str__ = lambda self: str.__str__(self)
 
 # Convenience shortcut
 load_mcc = File.load_mcc
+
+del (
+    io,
+    logging,
+    t,
+    zlib,
+)
