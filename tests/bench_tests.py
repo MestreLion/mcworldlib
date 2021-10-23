5f1568c9ab40018da4c0f8f0d5eeafe526678c7b
MestreLion <linux@rodrigosilva.com>
2021-10-22 22:27:51 -0300

    add tests dir

---
+++ tests/bench_tests.py
@@ -0,0 +1,197 @@
+import io
+import hashlib
+import json
+import time
+# import timeit  # Soon...
+import typing as t
+import zlib
+
+import msgpack
+import msgpack_numpy
+msgpack_numpy.patch()
+del msgpack_numpy
+import ujson
+import xxhash
+
+import mcworldlib as mc
+
+
+def measure(f: t.Callable, *a, **k):
+    """Lame wall time"""
+    def info(data):
+        out = f"{type(data)}"
+        try: out += f" ({len(data):,})"
+        except TypeError: pass
+        return out
+    t0 = time.time()
+    result = f(*a, **k)
+    t1 = time.time()
+    print(f"{t1 - t0: 8.4f}\t{f.__qualname__}\t-> {info(result)}")
+    return result
+
+
+def loadfile(path):
+    """Get disk I/O out of the equation"""
+    with open(path, 'rb') as f:
+        data = measure(f.read)
+    return io.BytesIO(data)
+
+
+# ------------------------------------------------------
+# Hashing
+
+# Slow for non-crypto, weak for crypto. Poor MD5!
+def md5(data: bytes) -> bytes:
+    return hashlib.md5(data).digest()
+
+
+# For xxHash, always using the one-shot versions,
+# faster than xxh*(data).digest()
+
+def xxh64(data: bytes) -> bytes:
+    return xxhash.xxh64_digest(data)
+
+
+def xxh3(data: bytes) -> bytes:
+    return xxhash.xxh3_64_digest(data)
+
+
+def xxh128(data: bytes) -> bytes:
+    return xxhash.xxh3_128_digest(data)
+
+
+def obj_hash(obj: object) -> int:
+    """Hash anything that contains only list, dict and hashable types"""
+    def freeze(o):
+        if isinstance(o, dict):
+            return frozenset({k: freeze(v) for k, v in o.items()}.items())
+        if isinstance(o, list):
+            return tuple([freeze(v) for v in o])
+        return o
+    return hash(freeze(obj))
+
+
+# --------------------------------------------------------------------------
+# Serializers
+
+def mpack(obj: object) -> bytes:
+    return msgpack.packb(obj)
+
+
+def write(tag: mc.AnyTag) -> bytes:
+    f = io.BytesIO()
+    tag.write(f)
+    return f.getvalue()
+
+
+def snbt(data: mc.AnyTag) -> str:
+    return str(data)
+
+
+def jsonobj(obj: object) -> str:
+    return json.dumps(obj)
+
+
+def jsontag(tag: mc.AnyTag) -> str:
+    obj = measure(unpack, tag)
+    return measure(json.dumps, obj)
+
+
+def ultrajson(obj: object) -> str:
+    return ujson.dumps(obj)
+
+
+def picking(_data: object) -> bytes: ...
+def walk(_tag: mc.AnyTag) -> t.Tuple: ...
+
+
+# --------------------------------------------------------------------------
+# Data Sources
+
+def load_region() -> mc.RegionFile:
+    filename = '../data/r.2.4.mca'
+    f = measure(loadfile, filename)
+    return measure(mc.RegionFile.parse, f, filename=filename)
+
+
+def load_mcc() -> mc.Chunk:
+    # Not using File.load_mcc() directly to decouple zlib and parse timings
+    data = measure(loadfile, '../data/c.-96.-8.mcc').getvalue()
+    data = measure(zlib.decompress, data)
+    return measure(mc.Chunk.parse, io.BytesIO(data))
+
+
+def unpack(tag: mc.AnyTag) -> object:
+    ...
+    return tag
+
+
+# --------------------------------------------------------------------------
+# Main
+
+def main():
+    print("Collecting data")
+    chunk: mc.Chunk = measure(load_mcc)
+    region: t.Dict[mc.ChunkPos, mc.Chunk] = measure(load_region)
+
+    # A "SuperCompound" of all region chunks just to test nbt.write
+    chunks: t.Dict[str, mc.RegionChunk]  # key = str(chunk.pos)
+    chunks = measure(mc.Compound, {str(k): v for k, v in region.items()})
+
+    print("\nSerializing tests")
+    for label, data in (("MCC Chunk", chunk), ("SuperCompound", chunks)):
+        print(label)
+        # Already ordered from Best to Worst
+        measure(mpack, data)
+        measure(write, data)
+        measure(snbt, data)
+
+    print("\nHashing tests")
+    for label, data in (("MCC Chunk",     mpack(chunk)),
+                        ("SuperCompound", mpack(chunks))):
+        print(label)
+        measure(xxh3, data)
+        measure(xxh128, data)
+        measure(xxh64, data)
+        measure(hash, data)
+        measure(md5, data)
+
+
+if __name__ == '__main__':
+    main()
+
+# Collecting data
+#   0.0015	BufferedReader.read	-> <class 'bytes'> (3,379,200)
+#   0.0015	loadfile	-> <class '_io.BytesIO'>
+#   0.0364	decompress	-> <class 'bytes'> (24,460,279)
+#   5.8933	Chunk.parse	-> <class 'mcworldlib.chunk.Chunk'> (2)
+#   5.9327	load_mcc	-> <class 'mcworldlib.chunk.Chunk'> (2)
+#   0.0027	BufferedReader.read	-> <class 'bytes'> (7,405,568)
+#   0.0028	loadfile	-> <class '_io.BytesIO'>
+#   1.5220	AnvilFile.parse	-> <class 'mcworldlib.anvil.RegionFile'> (1,024)
+#   1.5248	load_region	-> <class 'mcworldlib.anvil.RegionFile'> (1,024)
+#   0.0000	Compound	-> <class 'nbtlib.tag.Compound'> (1,024)
+#
+# Serializing tests
+# MCC Chunk
+#   0.2101	mpack	-> <class 'bytes'> (21,875,554)
+#   2.0058	write	-> <class 'bytes'> (24,460,279)
+#   4.0491	snbt	-> <class 'str'> (31,037,955)
+# SuperCompound
+#   0.1697	mpack	-> <class 'bytes'> (39,606,077)
+#   1.0542	write	-> <class 'bytes'> (5,089,152)
+#  38.8854	snbt	-> <class 'str'> (90,493,140)
+#
+# Hashing tests
+# MCC Chunk
+#   0.0012	xxh3	-> <class 'bytes'> (8)
+#   0.0013	xxh128	-> <class 'bytes'> (16)
+#   0.0022	xxh64	-> <class 'bytes'> (8)
+#   0.0083	hash	-> <class 'int'>
+#   0.0289	md5	-> <class 'bytes'> (16)
+# SuperCompound
+#   0.0022	xxh3	-> <class 'bytes'> (8)
+#   0.0023	xxh128	-> <class 'bytes'> (16)
+#   0.0032	xxh64	-> <class 'bytes'> (8)
+#   0.0150	hash	-> <class 'int'>
+#   0.0523	md5	-> <class 'bytes'> (16)
---
+++ tests/tests.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+
+import io
+import logging
+import os.path
+import sys
+
+import mcworldlib as mc
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-6s: %(message)s')
+
+
+def load_world(name='data/New World'):
+    """Common function for all methods"""
+    return mc.load(sys.argv[1] if len(sys.argv) > 1 else name)
+
+
+def load_region():
+    filename = os.path.join(mc.MINECRAFT_SAVES_DIR, 'New World/region/r.1.1.mca')
+    return mc.load_region(sys.argv[1] if len(sys.argv) > 1 else filename)
+
+
+def show_region(r=None):
+    r = r or load_region()
+    for _, c in enumerate(r.values()):
+        print(f'{c}\t{c!r}')
+    print(repr(r))
+    return r
+
+
+def write_chunk():
+    r = load_region()
+    with io.BytesIO() as f:
+        size = r[(0, 0)].write(f)
+        print(size)
+        print(f.getvalue())
+
+
+def write_region():
+    r = load_region()
+    with io.BytesIO() as f:
+        size = r.write(f)
+        print(size)
+        print(f.getvalue())
+
+
+def save_region():
+    r = show_region()
+    r.save('teste.mca')
+    r = mc.anvil.load_region('teste.mca')
+    show_region(r)
+
+
+def diamonds():
+    import os
+    from mcworldlib.anvil import RegionFile
+    from nbtlib import String
+    saves_dir = os.path.expanduser('~/.minecraft/saves/New World/region')
+    found = False
+    for file in os.listdir(saves_dir):
+        if not file.endswith('.mca'): continue
+        region = RegionFile.load(os.path.join(saves_dir, file))
+        sections = 0
+        for chunk in region.values():
+            for section in chunk['']['Level']['Sections']:
+                for block in section.get('Palette', []):
+                    if block['Name'] == 'minecraft:grass_block':
+                        block['Name'] = String('minecraft:diamond_block')
+                        if 'Properties' in block:
+                            del block['Properties']
+                        sections += 1
+                        found = True
+                        break
+        if sections:
+            print(f'Sections with grass blocks found in {file}')
+            region.save()
+    if found:
+        print(f'All grass blocks converted to diamonds. Happy "mining"!')
+
+
+def new_diamonds():
+    world = mc.load('New World')
+
+    for item in world['']['Data']['Player']['Inventory']:
+        item['id'] = mc.String('minecraft:diamond_block')
+        item['Count'] = mc.Byte(64)
+
+    world.save()
+
+    for region in world.regions.values():
+        found = False
+        for chunk in region.values():
+            for section in chunk['']['Level']['Sections']:
+                for block in section.get('Palette', []):
+                    if block['Name'] == 'minecraft:grass_block':
+                        block['Name'] = mc.String('minecraft:diamond_block')
+                        if 'Properties' in block:
+                            del block['Properties']
+                        found = True
+                        break
+        if found:
+            print(f'All grass blocks converted to diamonds. Happy "mining"!')
+            region.save()
+
+
+def entities():
+    r = load_region()
+    for c in r.values():
+        if c.data_root['Entities']:
+            print(repr(c))
+            print(repr(c.entities))
+            for e in c.entities:
+                print(e)
+            break
+
+
+def maps():
+    w = load_world('MestreLion')
+    r = w.regions[2, 1]
+    print(r[11, 22].data_root['TileEntities'][32]['Items'][5])
+
+
+def player_pos():
+    w = load_world()
+    print(w.player.get_chunk())
+
+
+def blocks(w=None):
+    if w is None:
+        w = mc.load('New World')
+
+    for chunk in [w.player.get_chunk()]:  # w.get_chunks(False):
+        print(chunk)
+        print()
+        for Y, palette, indexes in chunk.get_blocks():
+            print(f"SECTION Y={Y}")
+            for i, p in enumerate(palette):
+                bid = p['Name']
+                props = f" {p['Properties']}" if p.get('Properties') else ""
+                print(f"{i:2d} = {block_name(bid)} [{bid}]{props}")
+            print()
+            for y, sector_slice in enumerate(indexes, Y * mc.util.SECTION_HEIGHT):
+                print(f"y={y}")
+                print(sector_slice)
+                print()
+            print()
+        print()
+
+
+def block_name(bid):
+    return bid.split(':', 1)[-1].replace('_', ' ').title()
+
+
+def block_symbol(bid, length=3):
+    name = block_name(bid)
+    words = name.split()[:length]
+    symbol = "".join(_[0] for _ in words)
+    if len(words) < length:
+        symbol += words[-1][1:1 + length - len(words)]
+    return symbol
+
+
+def chests():
+    w = load_world('MestreLion')
+    for chunk in [w.player.get_chunk()]:  # w.get_chunks(False):
+        print(chunk)
+        print()
+        for pos in (
+            (-9000, 53, -1510),
+            (-8998, 53, -1512),
+            (-8996, 53, -1510),
+            (-8998, 53, -1508),
+            (-8998, 51, -1510),
+        ):
+            b = w.get_block_at(pos)
+            bid = b['Name']
+            props = f" {b['Properties']}" if b.get('Properties') else ""
+            print(f"{pos}: {block_name(bid)} [{bid}]{props}")
+
+
+def github():
+    world = load_world('MestreLion')
+    block = world.get_block_at((100, 60, 100))
+    print(block)
+
+
+def inventory_diamonds(world=None, save=False):
+    if not world: world = load_world()
+
+    inventory = world.player.inventory
+
+    # Easily loop each item as if the inventory were a list. In fact, it *is*!
+    for item in inventory:
+        print(f"Slot {item['Slot']:3}: {item['Count']:2} x {item['id']}")
+
+    free_slots = set(range(36)) - set(item['Slot'] for item in inventory)
+    free_slots = set(range(9, 36)) - set(item['Slot'] for item in inventory)
+    for slot in free_slots:
+        print(f"Adding 64 blocks of Diamond to inventory slot {slot}")
+        item = mc.Compound({
+            'Slot': mc.Byte(slot),
+            'id': mc.String('minecraft:diamond_block'),  # Sweet!
+            'Count': mc.Byte(64),  # Enough for you?
+        })
+        inventory.append(item)  # yup, it's THAT simple!
+
+    for slot in free_slots:
+        item = mc.Compound(
+            Slot=mc.Byte(slot),
+            id=mc.String('minecraft:diamond_block'),
+            Count=mc.Byte(64)
+        )
+        inventory.append(item)  # yup, it's THAT simple
+        print(f"Added to inventory: {item}")
+
+    if save:
+        world.save()
+
+
+def readme(world=None, save=False):
+    if not world: world = load_world()
+    inventory_diamonds(world=world, save=save)
+    chunks = world.entities[mc.Dimension.OVERWORLD][0, 0]
+    for chunk in chunks.values():
+        for entity in chunk.entities:
+            print(entity)
+
+
+def pretty():
+    world = load_world()
+    mc.pretty(world.regions)
+    for region in world.regions.items():
+        print(region)
+
+
+def lost_maps():
+    pass
+
+
+readme()
