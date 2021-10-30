30f7e45e4c5d2c04ba24e42d6207fcb715115cca
MestreLion <linux@rodrigosilva.com>
2021-10-30 12:09:25 -0300

    tree: improve typing and documentation

--- tests/tree.py
+++ tests/tree.py
@@ -5,46 +5,40 @@
 """Walk and tree-like print for nested, generic container structures
 
 The term Container is used here in a very broad, conceptual meaning: a collection
-of elements where each element can also be a Container of at least the same type,
-thus supporting arbitrarily deep (but finite) nesting.
+of elements where each element can also be a Container, thus supporting nesting.
 
-By these criteria, all Tuple-, List- and Dict-like structures qualify, while Strings
-and Sets don't, as those do not support nesting.
+By these criteria, all Tuple-, List- and Dict-like structures qualify, while Sets
+supports very limited element types. Strings could qualify, but as they support a
+single element type (themselves), cannot nest, and require special handling to
+avoid infinite recursion (1-char strings iterate themselves), they're usually not
+treated as a container.
 
-More formally put, a Container in this context is:
-- A Collection: sized via len()/__len__ and supporting iteration on elements.
-    - Mappings support this via .items() method, Sequences natively do so.
-- Indexable: supports retrieving an element by key via []/__getitem__.
-   - Not necessarily orderable, as key is not required to be an integer.
-   - Sets are not indexable, but could use enumerate() to satisfy this.
-- Discoverable: provides a way to iterate all its indexing keys
-    - Mappings do so natively, Sequences and Sets via enumerate()
-- Nestable: supports elements of the same type as itself.
-    - In other words: can be a (single) element of another Container
-    - Sets can not qualify this
-    - Strings (str/bytes) _could_ qualify, but that's a stretch. And interpreting
-      a single character as a Collection (of itself) leads to infinite nesting.
-- Distinguishable from a non-Container element, supporting non-Container elements
-    - Try to get away with THAT, Strings!
-- Not required to support mixed-type Containers, or even all Container types,
-  but it must at least support itself.
+More formally put, a Container in this context is any Collection, sized via len()
+and supporting iteration on elements and keys. Mappings support this via .items()
+method, Sequences natively do so via enumerate().
+
+Being such a Collection is the only actual requirement for walk(), but to be useful
+Containers should also be indexable, i.e., support retrieving an element by key
+via []. Sequences and Mappings are, Sequences having their integer index as key.
+Sets can be cast to Sequence to satisfy this.
 """
 
-from collections.abc import Mapping, Sequence, ByteString
+from collections.abc import Collection, Mapping, ByteString
 from typing import Callable, Tuple, Any, Iterator, Hashable, Union, NamedTuple, Iterable
-import typing as t  # for Sequence, Mapping, TypeAlias (Python 3.8+)
+import typing as t  # for Collection, Sequence and TypeAlias (Python 3.8+)
 
 # This non-sensical typing is just to illustrate the concepts
-Leaf:       't.TypeAlias' = Any  # A.K.A. Non-container, scalar, single value, atomic, etc
-Element:    't.TypeAlias' = Union[Leaf, 'Container']  # The reason `str` is not a Container
-Key:        't.TypeAlias' = Hashable  # Is always an int for Sequences
-Container:  't.TypeAlias' = Union[t.Sequence[Element], t.Mapping[Key, Element]]
+# The inability to have leaf elements is the main reason `str` is not a Container
+Leaf:       't.TypeAlias' = Any  # Non-container, scalar, single value, etc
+Element:    't.TypeAlias' = Union[Leaf, 'Container']
+Key:        't.TypeAlias' = Hashable  # For Sequences, always an int index
+Container:  't.TypeAlias' = t.Collection[Element]
 
 
 def _iter_container(container: Container) -> Iterable[Tuple[Key, Element]]:
-    """Default (key, element) iterable for generic containers
+    """Default (key, element) iterable for generic containers.
 
-    Handles any Sequence and Mapping
+    Handles Mappings via .items(), otherwise use enumerate().
     """
     if isinstance(container, Mapping):
         return container.items()
@@ -52,19 +46,20 @@ def _iter_container(container: Container) -> Iterable[Tuple[Key, Element]]:
 
 
 def _is_container(v: Element) -> bool:
-    """Default container test for generic containers
+    """Default container test for generic containers.
 
-    True for any Sequence or Mapping that is not a String
-    (as strings themselves
+    True for any Collection that is not a String (str/bytes).
     """
-    return (isinstance(v, (Sequence, Mapping)) and
-            not isinstance(v, (str, ByteString)))
+    return isinstance(v, Collection) and not isinstance(v, (str, ByteString))
 
 
-def get_element(root: Container, parts: Tuple) -> Element:
+def get_element(root: Container, parts: t.Sequence[Key]) -> Element:
+    """Retrieve an element from a deeply nested root container"""
     if not parts:
         return root
-    return get_element(root[parts[0]], parts[1:])
+    if not isinstance(root, Collection):
+        root = tuple(root)
+    return get_element(root[parts[0]], parts[1:])  # noqa
 
 
 class Item(NamedTuple):
@@ -137,10 +132,11 @@ def print_tree(root: Container, width: int = 2, offset: int = 0) -> None:
 
 
 def main():
-    import json
+    import json, mcworldlib as mc
     print_tree(json.load(open("../data/New World/advancements/"
                               "8b4accb8-d952-4050-97f2-e00c4423ba92.json")))
-    print_tree({((1, 2), "c", ((4, 5, 6), 7, "d"))})  # Works
+    print_tree(mc.load_dat("../data/New World/level.dat"))
+    print_tree([{((1, 2), "c", ((4, 5, 6), 7, "d"))}])
     print_tree("rodrigo")
 
 
